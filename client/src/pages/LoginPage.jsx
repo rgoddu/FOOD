ffcbe738bcc9fed7cc7baa875233375eae8f9076
christian goddu <rgoddu909@gmail.com>
2024-04-01 22:08:08 -0400

    refactored mui to native html

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,132 +1,73 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-import { useState } from 'react'
-import { useMutation } from '@apollo/client'
-import { LOGIN } from '../utils/mutations'
-import Auth from '../utils/auth'
-
-const defaultTheme = createTheme();
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { LOGIN } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 export default function LoginPage() {
-
-  const [formState, setFormState] = useState({ email: '', password: '' })
-  const [login, { error }] = useMutation(LOGIN)
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN);
 
   const handleChange = (event) => {
-    const { name, value } = event.currentTarget
-    setFormState({
-      ...formState, [name]: value
-    })
-  }
+    const { name, value } = event.target;
+    setFormState({ ...formState, [name]: value });
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
       const mutationResponse = await login({
         variables: { email: formState.email, password: formState.password }
-      })
-      const token = mutationResponse.data.login.token
-      Auth.login(token)
+      });
+      const token = mutationResponse.data.login.token;
+      Auth.login(token);
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
   };
 
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
-        <CssBaseline />
-        <Grid
-          item
-          xs={12}   // Set xs to 12 to fill the entire width on small screens
-          sm={4}
-          md={7}
-          sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
-            backgroundRepeat: 'no-repeat',
-            backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            display: 'flex',              // Add display flex
-            flexDirection: 'column',      // Set flex direction to column
-            height: '100%',               // Set height to 100%
-            width: '100vw',               // Set width to 100vw
-          }}
-        />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
-            </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
+    <div style={{ height: '100vh', display: 'flex' }}>
+      <div
+        style={{
+          backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+          backgroundRepeat: 'no-repeat',
+          backgroundColor: '#f5f5f5',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          flex: '1'
+        }}
+      />
+      <div style={{ flex: '1', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <div>
+          <h1 style={{ textAlign: 'center' }}>Sign in</h1>
+          <form onSubmit={handleSubmit}>
+            <div style={{ marginBottom: '1rem' }}>
+              <label htmlFor="email">Email Address</label>
+              <input
+                type="email"
                 id="email"
-                label="Email Address"
                 name="email"
-                autoComplete="email"
-                autoFocus
+                required
+                value={formState.email}
                 onChange={handleChange}
               />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
+            </div>
+            <div style={{ marginBottom: '1rem' }}>
+              <label htmlFor="password">Password</label>
+              <input
                 type="password"
                 id="password"
-                autoComplete="current-password"
+                name="password"
+                required
+                value={formState.password}
                 onChange={handleChange}
               />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign In
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                </Grid>
-                <Grid item>
-                  <Link href="/signup" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
-              </Grid>
-            </Box>
-          </Box>
-        </Grid>
-      </Grid>
-    </ThemeProvider>
+            </div>
+            <button type="submit" style={{ width: '100%', padding: '0.5rem', backgroundColor: '#1976D2', color: 'white', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>Sign In</button>
+          </form>
+          <p style={{ marginTop: '1rem', textAlign: 'center' }}>Don't have an account? <a href="/signup" style={{ color: '#1976D2' }}>Sign Up</a></p>
+        </div>
+      </div>
+    </div>
   );
 }
--- client/src/pages/SignUpPage.jsx
+++ client/src/pages/SignUpPage.jsx
@@ -1,146 +1,89 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
-import Auth from '../utils/auth'
-const defaultTheme = createTheme();
+import Auth from '../utils/auth';
 
 export default function SignUp() {
-
-  const [formState, setFormState] = useState({ email: '', password: '', firstName: '', lastName: '', phoneNumber: '' })
-  const [addUser] = useMutation(ADD_USER)
+  const [formState, setFormState] = useState({ email: '', password: '', firstName: '', lastName: '', phoneNumber: '' });
+  const [addUser] = useMutation(ADD_USER);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     const mutationResponse = await addUser({
-      variables: {
-        email: formState.email,
-        password: formState.password,
-        firstName: formState.firstName,
-        lastName: formState.lastName,
-        phoneNumber: formState.phoneNumber
-      }
-    })
-    const token = mutationResponse.data.addUser.token
-    Auth.login(token)
+      variables: formState
+    });
+    const token = mutationResponse.data.addUser.token;
+    Auth.login(token);
   };
 
   const handleChange = (event) => {
-    const { name, value } = event.currentTarget
+    const { name, value } = event.target;
     setFormState({
       ...formState,
       [name]: value
-    })
-  }
+    });
+  };
 
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                  onChange={handleChange}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                  onChange={handleChange}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                  onChange={handleChange}
-                />
-              </Grid><Grid item xs={12}>
-                <TextField
-                  fullWidth
-                  id="phone"
-                  label="Phone Number"
-                  name="phoneNumber"
-                  autoComplete="Phone Number"
-                  onChange={handleChange}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                  onChange={handleChange}
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="/" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-      </Container>
-    </ThemeProvider>
+    <div>
+      <div>
+        <h1>Sign up</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <input
+              type="text"
+              name="firstName"
+              required
+              placeholder="First Name"
+              value={formState.firstName}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="lastName"
+              required
+              placeholder="Last Name"
+              value={formState.lastName}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <input
+              type="email"
+              name="email"
+              required
+              placeholder="Email Address"
+              value={formState.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <input
+              type="tel"
+              name="phoneNumber"
+              placeholder="Phone Number"
+              value={formState.phoneNumber}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              name="password"
+              required
+              placeholder="Password"
+              value={formState.password}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
+      <div>
+        <p>Already have an account? <a href="/">Sign in</a></p>
+      </div>
+    </div>
   );
 }
